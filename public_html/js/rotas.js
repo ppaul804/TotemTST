aa1512024cd192ae8fd186f6ce4ba2ce16382e5e
Pedro Paul <ppaul804@gmail.com>
2018-01-09 17:27:05 -0200

    Separação das rotas do angula do main.js para rotas.js

---
+++ public_html/js/rotas.js
@@ -0,0 +1,36 @@
+/**
+* Inicializamos o angular.module com o mesmo nome que definimos na diretiva ng-app
+* E incluímos a dependência do ngRoute 
+*/
+var app = angular.module('toten', ['ngRoute', 'ngAnimate']);
+
+/**
+ * Como possuímos a variavel app definida acima com a inicialização do Angular
+ * através do app.config conseguimos criar as configurações
+ * definimos que essa configuração depende do $routeProvider e usamos na function($routeProvider)
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+
+        /* aqui dizemos quando estivernos na url / vamos carregar o conteúdo da pagina inicila a home
+         no segundo parametro definimos um objeto contendo o templateUrl da nossa pagina home e o controller que irá
+         preparar o conteudo e processar outros eventos da página que veremos posteriormente */
+        .when("/", {
+            templateUrl: "templates/home.html", 
+            controller: "HomeCtrl"
+        })
+        // configuração das rotas bem parecidas para as outras paginas
+        .when("/teste", {
+            templateUrl: "templates/teste.html",
+            controller: "TesteCtrl"
+        });
+}]);
+
+
+app.controller('HomeCtrl', function ($scope, $location) {
+    
+});
+ 
+app.controller('TesteCtrl', function ($scope, $location) {
+ 
+});
