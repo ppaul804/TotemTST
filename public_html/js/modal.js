afa3a401447fdd8f9cfbea651168d45e2c436c6f
Pedro Paul <ppaul804@gmail.com>
2018-01-29 16:04:19 -0200

    Ajuste no efeito de saida do pop-up de origin.html

--- public_html/js/modal.js
+++ public_html/js/modal.js
@@ -1,1 +1,9 @@
-$(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
+function testAnim(x) {
+    $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
+};
+$('#myModal').on('show.bs.modal', function (e) {
+      testAnim("zoomIn");
+})
+$('#myModal').on('hide.bs.modal', function (e) {
+      testAnim("zoomOut");
+})
