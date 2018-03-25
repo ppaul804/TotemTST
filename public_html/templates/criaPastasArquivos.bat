5bca068eb82d9f2808a30e41f7a6eeea65d71c2c
Pedro Paul <ppaul804@gmail.com>
2018-03-25 12:21:27 -0300

    Ajuste do conteudo

---
+++ public_html/templates/criaPastasArquivos.bat
@@ -0,0 +1,32 @@
+chcp 65001
+@echo off
+
+REM md caminho\da\pasta
+md 3_jt\noMund
+md 3_jt\noBras
+md 3_jt\ator
+md 4_tst\fatoHist
+md 4_tst\progInst
+md 4_tst\70AnosAcor
+md 5_csjt\cria
+md 5_csjt\csjtExPres
+md 5_csjt\memb
+md 6_curi\justNume
+md 6_curi\diciProf
+md 6_curi\voceSabi
+
+REM copy NUL caminho\da\pasta\aquirvo.html
+copy NUL 3_jt\noMund\noMund.html
+copy NUL 3_jt\noBras\noBras.html
+copy NUL 3_jt\ator\ator.html
+copy NUL 4_tst\fatoHist\fatoHist.html
+copy NUL 4_tst\progInst\fatoHist.html
+copy NUL 4_tst\70AnosAcor\70AnosAcor.html
+copy NUL 5_csjt\cria\cria.html
+copy NUL 5_csjt\csjtExPres\csjtExPres.html
+copy NUL 5_csjt\memb\memb.html
+copy NUL 6_curi\justNume\justNume.html
+copy NUL 6_curi\diciProf\diciProf.html
+copy NUL 6_curi\voceSabi\voceSabi.html
+
+pause
