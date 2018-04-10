2c09e04623725075ed69d5b05e56e1db88de3fe3
Pedro Paul <ppaul804@gmail.com>
2018-04-10 17:12:03 -0300

    Tentativa de ajuste em legiTrab.html

---
+++ public_html/templates/1_orig/Legislação Trabalhista/switch case de legiTrab.js
@@ -0,0 +1,262 @@
+switch (op) {
+    case 'l9_1830':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l108_10_1837':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l556_6_1850':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd2827_3_1879':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd1313_1_1891':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl4682_1_1923':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd16027_4_1923':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd4859_9_1924':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd4982_12_1925':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd5083_12_1926':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd17496_10_1926':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd5109_12_1926':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd19433_11_1930':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd19482_12_1930':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd19770_3_1931':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd21175_3_1932':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'c1934':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'c1937':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl1402_7_1939':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl1237_5_1939':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl2162_5_1940':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'jt_5_1941':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl5452_5_1943':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl9070_3_1946':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd9502_7_1946':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd41721_6_1957':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l4066_5_1962':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l4214_3_1963':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l4266_10_1963':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l4330_6_1964':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l4589_12_1964':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l4725_6_1965':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l5107_9_1966':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd229_2_1967':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'dl564_5_1969':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l5584_6_1970':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l5889_6_1973':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'c1988':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l7783_6_1989':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'd2100_12_1996':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'antd_2006':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l11948_6_2009':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l12551_12_2011':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l12513_10_2011':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l12440_7_2011':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l12382_2_2011':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l12506_10_2011':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l12761_12_2012':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'ec72_4_2013':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'mp665_12_2014':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l13467_6_2017':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+    case 'l13429_3_2017':
+        $.get("templates/1_orig/Legislação Trabalhista/modeloModal.html", function(content) {//variável content recebe o conteúdo
+            $(myModal).find('.modal-content').html(content);//põe o conteúdo na classe .modal-content
+        }, 'html');
+        break;
+}//fim switch
