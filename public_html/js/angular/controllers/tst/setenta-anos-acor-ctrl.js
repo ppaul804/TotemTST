596a7ae12845e95eb50cea7235944fdd1ae9eb1a
Pedro Paul <ppaul804@gmail.com>
2018-05-30 15:25:30 -0300

    setenta-anos-acor-ministros.json muito grande

--- public_html/js/angular/controllers/tst/setenta-anos-acor-ctrl.js
+++ public_html/js/angular/controllers/tst/setenta-anos-acor-ctrl.js
@@ -1,15 +1,15 @@
 //Fornece os dados para a view
 angular.module('toten')
     .controller('setentaAnosAcorCtrl', function ($scope, $http) {
-        $scope.foto = "conteudo/Origem e Evolução do Trabalho/Historia do Trabalho no Brasil/Foto/História do Trabalho no Brasil.jpg";
+        $scope.foto = "conteudo/TST/70 Anos em Acórdãos/Foto/70 Anos em Acórdãos.jpg";
         //$scope.legenda = "alguma legenda";
         //carrega um json de um arquivo externo. Caminho apartir de index.html
-        $http.get('js/angular/controllers/origEvol/hist-trab-subtopicos.json')
+        $http.get('js/angular/controllers/tst/setenta-anos-acor-ministros.json')
             .then(function (resultado) {
-                $scope.subtopicos = resultado.data;
+                $scope.lista = resultado.data;
             });
 
         //Conteúdo da pagina (view)
-        $scope.titulo = "setentaAnosAcorCtrl";
-        $scope.texto = "A história do trabalho no Brasil, marcada pelo longo período de escravidão e exploração dos trabalhadores, é muito rica em informações, há diversas formas de relações trabalhistas, movimentos sociais, lutas, direitos e legislações. Contudo, entre o período colonial até a proteção estabelecida pela Consolidação das Leis do Trabalho (CLT) e pela Justiça do Trabalho (JT), muitos embates ideológicos, greves e até mesmo conflitos foram travados. Os tipos de trabalho se desenvolveram e mudaram, as relações de trabalho se modificaram e se adaptaram a cada novo cenário.	Em síntese, apresentamos aqui importantes eventos da história do nosso país, de nossos trabalhadores e do desenvolvimento social e econômico da nação. Dividido nas principais formas de governo a que o Brasil foi submetido (Colônia, Império e República), o conteúdo apresentado dará um panorama rápido do que foi a história do trabalho brasileiro e do caminho traçado até chegarmos à criação da Justiça do Trabalho.";
+        $scope.titulo = "70 Anos em Acórdãos";
+        $scope.texto = "Os acórdãos aqui selecionados demonstram um pouco da história, da importância e da formação da jurisprudência do Tribunal Superior do Trabalho ao longo do tempo, reunindo as principais decisões do TST nas últimas décadas. As informações são baseadas no livro institucional intitulado “Tribunal Superior do Trabalho – 70 anos em acórdãos”. A publicação foi coordenada pelo ministro Alexandre Agra Belmonte em 2016, na qual destacam-se algumas participações por meio de acórdãos representativos aos 70 anos da Corte superior trabalhista. Foi produzida pela Editora JC e pelo Instituto Justiça e Cidadania. Organizada por ministros do TST antigos e atuais. A compilação retrata, por meio das decisões, a evolução na interpretação das questões trabalhistas, revelando o papel da Justiça do Trabalho na consolidação e na harmonização dos princípios fundamentais do valor social do trabalho e da livre iniciativa, que informam o Estado Democrático de Direito.";
     });
